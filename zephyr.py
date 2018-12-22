c01de3bec2fe311ccf8c18c496f5cdf48f82aa61
nikolaychechelev <nikolaychechelev@gmail.com>
2018-12-22 05:16:33 +0400

    commit

--- zephyr.py
+++ zephyr.py
@@ -1,1 +1,14 @@
-import pygame
+import thread
+from Display import Screen
+
+s = Screen()   # creating Screen Object
+def new_data(*args):  # procedure of infinitive data loading
+    while 1:
+        s.load_data()
+
+s.main_screen()  # Drawing main Sreen
+thread.start_new(new_data, (None,))  # Start data loading
+s.loop() # main cycle
+
+
+
